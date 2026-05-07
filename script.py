a76765d34df31585ba4449d260b8ef53339d5c5f
joelin43 <joelrovira20@gmail.com>
2026-05-07 22:01:05 +0200

    Uso de try con exepciones custom

--- script.py
+++ script.py
@@ -4,81 +4,128 @@ import sys
 import subprocess
 # Para poder usar expresiones regulares
 import re
+# Para crear hilos aparte del programa principal
+import threading
+# Para usar el sleep
+import time
 
-def argument():
-    
+
+
+def spinner_animacion():
+    # Los caracteres que rotarán
+    caracteres = ['[-]', '[\\]', '[|]', '[/]']
+    i = 0
+    while animar:
+        # \r vuelve al inicio, end="" evita el salto de línea
+        sys.stdout.write(f"\r{caracteres[i % len(caracteres)]} Cargando...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+        i += 1
+
+
+try:
+############### Arguments ###############
     if len(sys.argv) == 2:
-        return True
-    elif len(sys.argv) > 2:
-        print("To many arguments, example: script.py [x.x.x.x]")
-        return False
-    else:
-        print("The program requires an argument: script.py [x.x.x.x]")
-        return False
+        ip = sys.argv[1]
 
-def correctIp(ip):    
-    
+    elif len(sys.argv) > 2:
+        raise Exception ("To many arguments, example: script.py [x.x.x.x]")
+    else:
+        raise Exception ("The program requires an argument: script.py [x.x.x.x]")
+
+############### Correct Ip ###############
     ip_separada = ip.split(".")
 
     for i in ip_separada:
         i = int(i)
         if i > 255:
-            return False
-            break
-        else:
-            return True
+            raise Exception ("The ip isn't correct")
+ 
+############### Ping ###############
+    animar = True
+    hilo = threading.Thread(target=spinner_animacion)
+    hilo.start()
 
-
-if argument():
-    
-    ip = sys.argv[1]
-
-    if correctIp(ip):
-        #Hacemos ping
+    try:
         ping = subprocess.run(["ping", ip, "-c 1"], capture_output=True, text=True)
 
-        if ping.returncode == 0:
-            print("Bien")
-        elif ping.returncode == 1:
-            print("Mal")
+    finally:
+        # Detener la animación
+        animar = False
+        hilo.join()
 
+        if ping.returncode == 1:
+            raise Exception ("\r[X] There's not conection with the machine")
+    
+    print(("\r[✓] Ping correcto"))
 
-########## Ping ##########
+############### Sistema Operativo ###############
+    #Separamos los resultados
+    ping = ping.stdout.split()
 
+    #Sacamos el valor del ttl
+    ping = ping[12]
 
+    #Esta expresion regular busca: \d cualquier numero, {1,3} que tenga entre 1 y 3 digitos
+    ttl = int(re.findall(r"\d{1,3}", ping)[0])
 
+    #Comprobamos que sistema es por su ttl
+    if ttl >= 0 and ttl <= 64:
+        print("Linux")
+    elif ttl >= 65 and ttl <= 128:
+        print("Windows")
+    else:
+        print("Not found")
+            
+    animar = True
+    hilo = threading.Thread(target=spinner_animacion)
+    hilo.start()
 
-#Separamos los resultados
-ping = ping.stdout.split()
-
-#Sacamos el valor del ttl
-ping = ping[12]
-
-#Esta expresion regular busca: \d cualquier numero, {1,3} que tenga entre 1 y 3 digitos
-ttl = int(re.findall(r"\d{1,3}", ping)[0])
-
-#Comprobamos que sistema es por su ttl
-if ttl >= 0 and ttl <= 64:
-    print("Linux")
-elif ttl >= 65 and ttl <= 128:
-    print("Windows")
-else:
-    print("Not found")
-
+    try:
 ########## NMAP ##########
-escaneo = subprocess.run(["nmap", "-p-", "--open", "-sS", "--min-rate", "5000", "-n", "-Pn", ip], capture_output=True, text=True)
+        escaneo = subprocess.run(["nmap", "-p-", "--open", "-sS", "--min-rate", "5000", "-n", "-Pn", ip], capture_output=True, text=True)
 
-escaneo = escaneo.stdout
+        escaneo = escaneo.stdout
 
-# (\d{1,5}) captura un número entre 1 y 5 cifras, /tcp justo después debe haber “/tcp”, \s+open uno o más espacios y luego la palabra “open”
-encontrados = re.findall(r"(\d{1,5})/tcp\s+open", escaneo)
+        # # (\d{1,5}) captura un número entre 1 y 5 cifras, /tcp justo después debe haber “/tcp”, \s+open uno o más espacios y luego la palabra “open”
+        encontrados = re.findall(r"(\d{1,5})/tcp\s+open", escaneo)
 
-# Toma solo la parte numérica antes del /
-puertos = [e.split("/")[0] for e in encontrados]
+        # # Toma solo la parte numérica antes del /
+        puertos = [e.split("/")[0] for e in encontrados]
 
-# Une con comas
-puertos =  ",".join(puertos)
+        # # Une con comas
+        puertos =  ",".join(puertos)
 
-print(puertos)
+        print(puertos)
+# Guardar output  en archivo (Open)
+        escaneo2 = subprocess.run(["nmap", "-p", puertos, "-sCV", ip, "-oN targeted"], capture_output=True, text=True)
 
-escaneo2 = subprocess.run(["nmap", "-p", puertos, "-sCV", ip, "-oN targeted"], capture_output=True, text=True)
+    finally:
+        # Detener la animación
+        animar = False
+        hilo.join()
+        print("\r Escaneo completado ")
+
+
+
+except Exception as e:
+
+    print(e)
+
+    # ########## NMAP ##########
+    # escaneo = subprocess.run(["nmap", "-p-", "--open", "-sS", "--min-rate", "5000", "-n", "-Pn", ip], capture_output=True, text=True)
+
+    # escaneo = escaneo.stdout
+
+    # # (\d{1,5}) captura un número entre 1 y 5 cifras, /tcp justo después debe haber “/tcp”, \s+open uno o más espacios y luego la palabra “open”
+    # encontrados = re.findall(r"(\d{1,5})/tcp\s+open", escaneo)
+
+    # # Toma solo la parte numérica antes del /
+    # puertos = [e.split("/")[0] for e in encontrados]
+
+    # # Une con comas
+    # puertos =  ",".join(puertos)
+
+    # print(puertos)
+
+    # escaneo2 = subprocess.run(["nmap", "-p", puertos, "-sCV", ip, "-oN targeted"], capture_output=True, text=True)
